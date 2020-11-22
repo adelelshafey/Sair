478d39fee950ada94dce840856e5ac0d02df040b
unknown <devadelwork@gmail.com>
2020-11-23 00:24:13 +0200

    deploy

---
+++ js/chunk-2d0bfe79.4b7cc86a.js
@@ -0,0 +1,2 @@
+(window["webpackJsonp"]=window["webpackJsonp"]||[]).push([["chunk-2d0bfe79"],{4007:function(e,n,t){"use strict";t.r(n);var a=function(){var e=this,n=e.$createElement;e._self._c;return e._m(0)},l=[function(){var e=this,n=e.$createElement,t=e._self._c||n;return t("div",[t("h1",[e._v("blank page")])])}],c={name:"blank"},r=c,u=t("2877"),s=Object(u["a"])(r,a,l,!1,null,null,null);n["default"]=s.exports}}]);
+//# sourceMappingURL=chunk-2d0bfe79.4b7cc86a.js.map
